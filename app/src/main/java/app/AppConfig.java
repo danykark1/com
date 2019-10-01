66ce03dc9b6dbd801c7e5f70e31361eb06db8582
danykark1 <dkarake@caa.gov.rw>
2019-10-01 11:19:14 +0200

    first commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,9 @@
+package app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://caa.gov.rw/mob/android/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://caa.gov.rw/mob/android/register.php";
+}
